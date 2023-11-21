4163cedc8ffcf6b9ceb289eb3ddda642f158c294
Adam Kuszczyński <kuszczynski.adam@gmail.com>
2023-11-21 18:20:22 +0000

    front

---
+++ front/src/views/StartView.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+ <div class="header clearfix" >
+   Zadzwonimy do Ciebie w ciągu 26 sekund.
+ </div>
+ <label class="form-label clearfix"  for="form-number">
+   Wprowadź numer
+ </label>
+ <input v-model="number"
+  class="form-number clearfix"  id="form-number"/>
+ <div class="call-button"  @click="call()">
+   Zadzwoń teraz
+ </div>
+</div>
+</template>
