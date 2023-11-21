120d4ec98faa111c117333e9dd4127020204ca34
Adam Kuszczyński <kuszczynski.adam@gmail.com>
2023-11-21 18:37:53 +0000

    update

--- front/src/views/StartView.vue
+++ front/src/views/StartView.vue
@@ -13,3 +13,25 @@
  </div>
 </div>
 </template>
+<script>
+export default {
+ data() {
+   return {
+     number: ''
+   }
+ },
+ methods: {
+   async call() {
+     let responseStream = await fetch(`${process.env.VUE_APP_SERVER_URL}/call`, {
+       method: "POST",
+       headers: {
+         "Content-type": "application/json; charset=UTF-8"
+       },
+       body: JSON.stringify({number: this.number})
+     });
+let response = await responseStream.json()
+this.$router.push({ name: 'ringing', params: { callsId: response.id }})
+   },
+ },
+}
+</script>
