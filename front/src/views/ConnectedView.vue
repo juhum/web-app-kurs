9bc2dac23016629e7d01954b22ab6eb5de57ae8e
Adam Kuszczyński <kuszczynski.adam@gmail.com>
2023-11-28 19:24:20 +0000

    call time counter

--- front/src/views/ConnectedView.vue
+++ front/src/views/ConnectedView.vue
@@ -1,7 +1,42 @@
 <template>
-<div class="box">
- <div class="text">
-    Trwa połączenie...
- </div>
-</div>
+  <div class="box">
+    <div class="text">
+      Trwa połączenie... 
+      <br>
+      {{ formattedTime }}
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      timeLeft: 0,
+      formattedTime: "00:00",
+    };
+  },
+  mounted() {
+    this.startTimer();
+  },
+  methods: {
+    startTimer() {
+      this.timerInterval = setInterval(() => {
+        this.timeLeft++;
+        this.formatTime();
+      }, 1000);
+    },
+    formatTime() {
+      const minutes = Math.floor(this.timeLeft / 60);
+      const seconds = this.timeLeft % 60;
+      this.formattedTime = `${this.padNumber(minutes)}:${this.padNumber(seconds)}`;
+    },
+    padNumber(number) {
+      return number.toString().padStart(2, '0');
+    },
+  },
+  beforeUnmount() {
+    clearInterval(this.timerInterval);
+  },
+};
+</script>
